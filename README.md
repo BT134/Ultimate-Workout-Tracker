9bc724849d8c144cd10a0d8717933f8d38e82353
Brenton Turnor <brenton.turnor@hotmail.com>
2021-10-06 16:41:01 +0800

    updating routes and readme

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # Ultimate-Workout-Tracker
-A workout tracker where you will be able to create and track daily workouts. Utilizing MongoDB, Express.js, Node.js and Mongoose.
+
+![Github licence](http://img.shields.io/badge/license-MIT-blue.svg)
+## Description
+
+> 
+
+ ## Table of Contents 
+  - [Description](#description)
+  - [User Story](#user-story)
+  - [Installation](#installation)
+  - [Visual Demo](#visual-demo)
+  - [Tests](#tests)
+  - [Built With](#built-with)
+  - [Acknowledgements](#acknowledgements)
+  - [Author](#author)
+  - [Contact](#contact)
+
+## User Story
+```
+As a user, I want to be able to view create and track daily workouts. I want to be able to log multiple exercises in a workout on a given day. I should also be able to track the name, type, weight, sets, reps, and duration of exercise. If the exercise is a cardio exercise, I should be able to track my distance traveled.
+```
+
+## Installation/Usage
+
+> The user should clone the repository from GitHub. This application requires Node.js, Express.js, Sequelize and Mysql2. To connect to the MYSQL database run `mysql -u root -p` and enter your password from .env file. Then source the schema.sql. Then seed the data by running `npm run seed`. To start application run `npm install` then once depencies have installed run `npm start`.
+
+> This application can be viewed [HERE]().
+
+## Visual Demo
+
+><img src="" alt= "">
+
+> 
+
+## Tests
+
+> There are no tests for this app.
+
+## Built With
+
+> [Visual Studio Code](https://code.visualstudio.com/)
+
+## Acknowledgements
+
+* [GitHub Pages](https://pages.github.com)
+* [MDN Web Docs](https://developer.mozilla.org/en-US/)
+* [NodeJS](https://nodejs.org/en/)
+* [npm](https://www.npmjs.com/)
+* [Express.js](https://expressjs.com/)
+* [MongoDB](https://www.mongodb.com/)
+* [Mongoose](https://mongoosejs.com/docs/)
+
+## Author
+
+> Brenton Turnor - [https://github.com/BT134](https://github.com/BT134)
+
+## Contact 
+
+> Brenton Turnor [@BTurnor](https://twitter.com/BTurnor) - brenton.turnor@hotmail.com
+
+> Profile: [https://bt134.github.io/bts-portfolio/](https://bt134.github.io/bts-portfolio/)
+
--- routes/api.js
+++ routes/api.js
@@ -1,59 +1,73 @@
 const router = require("express").Router();
-const db = require("../models");
+const Workout = require("../models/workout.js");
 
 //get workouts
 router.get("/api/workouts", (req, res) => {
-    db.Workout.aggregate([{
-            "$addFields": {
-                "totalDuration": {
-                    "$sum": "$exercises.duration"
+    Workout.aggregate([{
+            $addFields: {
+                totalDuration: {
+                    $sum: "$exercises.duration"
                 }
             }
-    }]).then((result) => {
+    }])
+    .then((result) => {
         res.json(result)
     })
+    .catch((err) => {
+        res.status(400).res.json(err);
+    });
 });
 
 // add exercise
 router.put("/api/workouts/:id", (req, res) => {
-    db.Workout.findByIdAndUpdate(req.params.id, {
-        "$push": {
-            "exercises": req.body
+    Workout.findByIdAndUpdate(req.params.id, {
+        $push: {
+            exercises: req.body
         }
-    }, {new: true})
+    }, {new: true, runValidators: true })
     .then((dbWorkout) => {
         res.json(dbWorkout);
     })
-
+    .catch((err) => {
+        res.status(400).res.json(err);
+      });
 });
 
 //create workout
 router.post("/api/workouts", ({ body }, res) => {
-    db.Workout.create(body)
+    Workout.create(body)
     .then((dbWorkout => {
         res.json(dbWorkout);
-    })).catch(err => {
-        res.json(err);
+    }))
+    .catch((err) => {
+        res.status(400).res.json(err);
     });
 });
 
 // get workouts in range
 router.get("/api/workouts/range", (req, res) => {
-    db.Workout.aggregate([{ 
-        "$sort" : { day : -1} 
+    Workout.aggregate([{ 
+        $sort : { day : -1} 
      },
      { 
-         "$limit" : 7 
+         $limit : 7 
      },
      {
-         "$addFields": {
-             "totalDuration": {
-                 "$sum": "$exercises.duration"
-             }
+         $addFields: {
+            totalDuration: {
+                 $sum: "$exercises.duration"
+            },
+            totalWeight: { 
+                 $sum: '$exercises.weight' 
+            },
          }
- }]).then((result) => {
-         res.json(result)
-     })
+    }])
+    .then((result) => {
+        res.json(result)
+    })
+    .catch((err) => {
+        res.status(400).res.json(err);
+    }); 
  });
 
 
